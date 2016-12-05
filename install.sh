d5ba397fbb9dab9930377e3696748924193ac87b
Burak Serpici <bserpici@gmail.com>
2016-12-05 13:52:46 +0300

    Termite config added

--- install.sh
+++ install.sh
@@ -5,3 +5,4 @@ echo "Installing dotfiles"
 ln -s ~/dotfiles/nano/.nanorc ~/.nanorc
 ln -s ~/dotfiles/tmux/.tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/zsh/.zshrc ~/.zshrc
+ln -s ~/dotfiles/termite/config ~/.config/termite/config
